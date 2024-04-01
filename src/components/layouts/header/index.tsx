769cb3696d08b536e7a9bd7b961b97a0c817dbb4
Sternels <sternel888@gmail.com>
2024-04-01 12:02:05 +0900

    fix

--- src/components/layouts/header/index.tsx
+++ src/components/layouts/header/index.tsx
@@ -1,19 +1,25 @@
-import { FC, useState } from "react";
+import { FC, useEffect, useState } from "react";
 import cart from "assets/icons/cart.svg";
 import favourites from "assets/icons/favourites.svg";
-import { useAppSelector } from "app/store/hooks";
+import { useAppDispatch, useAppSelector } from "app/store/hooks";
 import { ProductList } from "components/modules/cart/list";
 import { Link } from "react-router-dom";
+import { Button } from "components/ui/form/button";
+import { getOrdersAction } from "app/store/actions/order";
 
 import styles from "./styles.module.scss";
-import { Button } from "components/ui/form/button";
 
 export interface IHeaderProps {}
 
 export const Header: FC<IHeaderProps> = ({}) => {
+	const dispatch = useAppDispatch();
 	const [isOpen, setIsOpen] = useState<boolean>(false);
 
 	const { count, mainPrice } = useAppSelector((state) => state.orderReducer);
+
+	useEffect(() => {
+		dispatch(getOrdersAction());
+	}, []);
 
 	return (
 		<div className={styles.header}>
--- src/components/modules/cart/index.tsx
+++ src/components/modules/cart/index.tsx
@@ -1,13 +1,12 @@
 import { FC } from "react";
 import { ProductList } from "./list";
 import { PaymentInformation } from "./payment_information";
-import { CartHOC } from "hocs";
 
 import styles from "./styles.module.scss";
 
 export interface ICartBaseProps {}
 
-export const CartBase: FC<ICartBaseProps> = ({}) => {
+export const Cart: FC<ICartBaseProps> = ({}) => {
 	return (
 		<>
 			<h2>Корзина</h2>
@@ -19,5 +18,3 @@ export const CartBase: FC<ICartBaseProps> = ({}) => {
 	);
 };
 
-
-export const Cart = CartHOC(CartBase) as typeof CartBase;
